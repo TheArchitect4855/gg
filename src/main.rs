69058d7c7f43b4fe10e5a69ce2003a14b0106cc7
Kurtis Knodel <kurtis@kurtisknodel.com>
2024-08-08 12:10:37 +0200

    Set up logging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+	init_logging();
+
 	let addr = if cfg!(debug_assertions) {
 		("127.0.0.1", 8080)
 	} else {
@@ -10,9 +12,14 @@ async fn main() -> std::io::Result<()> {
 
 	HttpServer::new(|| {
 		let v1 = gg::app::v1::init(web::scope("/v1"));
-		App::new().service(v1)
+		App::new().wrap(Logger::default()).service(v1)
 	})
 	.bind(addr)?
 	.run()
 	.await
+}
+
+fn init_logging() {
+	let env = env_logger::Env::new().filter_or("LOG_LEVEL", "info");
+	env_logger::init_from_env(env);
 }
